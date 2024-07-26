cf0552bd79908bca84e90b10da7aaf4554020e20
GHimejima <ghimejima24@gmail.com>
2024-07-26 22:59:51 +0530

    Add files via upload

---
+++ src/app/api/transfer-sol/const.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
+  "5ee3NLjWfNTBw9AbuY2LmvZGdah1rXE1DtpcMMeFjAXE",
+);
+
+export const DEFAULT_SOL_AMOUNT: number = 1.0;
